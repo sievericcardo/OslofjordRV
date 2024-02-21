79df29e7952633fbfbcbe88e70b57b1099e97579
Vebjørn Olsen Leihne <VebjornLeihne@eduroam-193-157-252-193.wlan.uio.no>
2024-02-21 15:18:53 +0100

    the sql can be used in hasura to more easily create tables for runtime monitoring result

---
+++ create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS runtime_monitoring (
+    record_number int,
+    suitable_temperature boolean,
+    suitable_spawning_temperature boolean,
+    preferred_spawning_temperature boolean
+);
