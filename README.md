9bd30be35f192d16b548b744324a38763f58c554
Vebjørn Olsen Leihne <vebjorol@uio.no>
2024-02-23 12:41:17 +0100

    small fixes

--- README.md
+++ README.md
@@ -44,5 +44,6 @@ Offline monitoring of data from the Oslofjord Database.
 		record_number int NOT NULL UNIQUE,
 		suitable_temperature boolean,
 		suitable_spawning_temperature boolean,
-		preferred_spawning_temperature boolean
+		preferred_spawning_temperature boolean,
+		PRIMARY KEY (record_number)
 	);
--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+echo "Creating TeSSLa specification..."
 python3 build_spec.py
+echo "Instrumenting and compiling C-code..."
 java -jar tessla.jar instrumenter spec.tessla main.c /usr/lib/gcc/x86_64-linux-gnu/11/include/
 gcc main.c.instrumented.c -llogging -lcurl -lcjson -pthread -ldl -o main
+echo "Fetching data and generating a trace..."
 ./main
+echo "Running the TeSSLa monitor on the trace..."
 java -jar tessla.jar interpreter spec.tessla trace.log > output.out
+echo "Posting the monitor output to the database..."
 gcc post_data.c -lcurl -o post_data && ./post_data
