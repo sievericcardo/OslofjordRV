49182e25dfa62eba5ef7394a82c89d7a44d3ee75
Vebjørn Olsen Leihne <vebjorol@uio.no>
2024-03-06 20:03:11 +0100

    cleaned up

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-java -jar tessla.jar instrumenter spec.tessla main.c /usr/lib/gcc/x86_64-linux-gnu/11/include/ && gcc main.c.instrumented.c -llogging -lcurl -lcjson -pthread -ldl -o main
+java -jar tessla.jar instrumenter spec.tessla main.c /usr/lib/gcc/x86_64-linux-gnu/11/include/
+gcc main.c.instrumented.c -llogging -lcjson -pthread -ldl -o main
