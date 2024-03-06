db17c5a4fb4aad700b02098fe0355c453fe675dd
Vebjørn Olsen Leihne <vebjorol@uio.no>
2024-03-06 20:03:26 +0100

    cleaned up

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Fetching data from knowledgegraph and building TeSSLa specification..."
-python3 build_spec.py "$1"
-echo "Fetching simulation data and generating a trace..."
+echo "Fetching..."
+python3 get_data.py $1 "$2"
+echo "Parsing..."
 ./main
-echo "Running the TeSSLa monitor on the trace..."
+echo "Monitoring..."
 java -jar tessla.jar interpreter spec.tessla trace.log > output.out
-echo "Posting the monitor output to the database..."
-./post_data
+echo "Posting..."
+python3 post_data.py $1 "$2"
 echo "Done!"
