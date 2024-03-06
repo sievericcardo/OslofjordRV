6ed9676666b46c6ebb27e5092578ff79b496859d
Vebjørn Olsen Leihne <vebjorol@uio.no>
2024-03-06 18:15:53 +0100

    python file now also queries the simulation table

--- build_spec.py
+++ build_spec.py
@@ -1,18 +1,42 @@
 import sys
+import json
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
-transport = AIOHTTPTransport(
-        url="http://localhost:8080/v1/graphql",
-        headers={"Content-Type":"application/json","x-hasura-admin-secret":"mylongsecretkey"}
-)
 
+
+#Set up GQL with url and headers
+transport = AIOHTTPTransport(
+    url="http://localhost:8080/v1/graphql",
+    headers={"Content-Type":"application/json","x-hasura-admin-secret":"mylongsecretkey"}
+)
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
-query = gql(
-    f'''
-    query fish_info {{
-        fishFields(name: "{sys.argv[1]}") {{
+
+
+#Query the simulation table for the data we want to monitor
+sim_query = gql(f'''
+	query sim_query {{
+		simulations(where: {{grid_id: {{_eq: {sys.argv[1]} }} }}) {{
+			id_sim
+			grid_id
+			temperature
+		}}
+	}}
+''')
+sim_response = client.execute(sim_query)
+
+#Write response to file
+f = open("sim_response.json", "w")
+json.dump(sim_response, f);
+f.close()
+
+
+
+#Query the knowledgegraph for values about the species we want to check
+species_query = gql(f'''
+    query species_query {{
+        fishFields(name: "{sys.argv[2]}") {{
             prefMinSpawnTemp
             prefMaxSpawnTemp
             name
@@ -22,13 +46,11 @@ query = gql(
             maxSpawnTemp
         }}
     }}
-    '''
-)
+''')
+species_response = client.execute(species_query)
 
-result = client.execute(query)
-
-#Isolate the values we are interested in, and format them
-items = result["fishFields"][0]
+#Isolate the values we are interested in, and format them according to TeSSLa syntax
+items = species_response["fishFields"][0]
 for x in items:
 	if isinstance(items[x], int):
 		items[x] = float(items[x])
@@ -36,15 +58,12 @@ for x in items:
 			s = str(items[x]).split("-")[1]
 			items[x] = f"-.{float(s)}"
 
-
 #Write the TeSSLa specification
 f = open("spec.tessla", "w")
 f.write("@InstFunctionCallArg(\"my_func\", 0)\n")
 f.write("in temperature: Events[Float]\n\n")
 
-
 counter = 0
-
 
 #Check if suitable temperature
 if items["maxTemp"] != None and items["minTemp"] != None:
@@ -53,7 +72,6 @@ if items["maxTemp"] != None and items["minTemp"] != None:
 	minTemp = items["minTemp"]
 	f.write(f"def suitable_temperature = temperature <=. {maxTemp} && temperature >=. {minTemp}\n")
 	f.write(f"out suitable_temperature\n\n")
-
 
 #Check if suitable spawning temperature
 if items["maxSpawnTemp"] != None and items["minSpawnTemp"] != None:
@@ -72,12 +90,10 @@ if items["prefMaxSpawnTemp"] != None and items["prefMinSpawnTemp"] != None:
 	f.write(f"def preferred_spawning_temperature = temperature <=. {float(prefMaxSpawnTemp)} && temperature >=. {float(prefMinSpawnTemp)}\n")
 	f.write(f"out preferred_spawning_temperature\n\n")
 
-
 #If the species doesn't have any of the values we want to check in the knowledge graph
 if counter == 0:
 	f.close()
 	f = open("spec.tessla", "w")
-
 
 #Close out file
 f.write(f"@InstFunctionCallArg(\"my_func\", 1)\n")
