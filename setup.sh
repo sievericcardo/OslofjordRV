5ddf36e6f4521d4ccb99eaef8c5293a8825fcc48
Vebjørn Olsen Leihne <vebjorol@uio.no>
2024-03-20 08:47:01 +0100

    deleted setup.sh

--- setup.sh
+++
@@ -1,3 +0,0 @@
-sudo apt install libcjson-dev build-essential unzip default-jre python3 python3-pip
-python3 -m pip install gql[all]
-wget https://www.tessla.io/logging.zip && unzip logging.zip && rm logging.zip && sudo mv liblogging.a /usr/lib && sudo mv logging.h /usr/include
