38d29868487557073f506683c38dcbec8f3cd113
admin <admin@admin.com>
2024-04-10 10:31:42 +0000

    now handles changes monitor output length

--- post_data.py
+++ post_data.py
@@ -4,7 +4,7 @@ from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
 
-#Set up GQL with url and headers
+#Set up GQL with url and headers.
 transport = AIOHTTPTransport(
     url="http://localhost:8080/v1/graphql",
     headers={"Content-Type":"application/json","x-hasura-admin-secret":"mylongsecretkey"}
@@ -12,17 +12,29 @@ transport = AIOHTTPTransport(
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
 
-#Read monitor output to a string
+#Read monitor output to a string.
 f = open("output.out", "r")
 lines = f.readlines()
 f.close()
 
-count = 0
 start_mutation = f'mutation MyMutation {{insert_runtime_monitoring(objects: {{request_id: {sys.argv[1]}, '
 mutation = start_mutation
 
 
-#Go through monitor output, build mutation, and post to database
+#The length of the monitor output determines how many lines a "row" spans. The output length depends on the info available about the species.
+count = 0
+limit = 0
+if len(lines) == 24:
+    limit = 1
+elif len(lines) == 24*2:
+    limit = 2
+elif len(lines) == 24*3:
+    limit = 3
+elif len(lines) == 24*4:
+    limit = 4
+
+
+#Go through monitor output, build mutation, and post to database.
 for line in lines:
     count += 1
 
@@ -42,13 +54,13 @@ for line in lines:
     elif key == "preferred_spawning_temperature":
         mutation += f'preferred_spawning_temperature: {value}, '
 
-    if count == 4:
+    if count == limit:
         mutation += '}) {affected_rows}}'
         rv_response = client.execute(gql(mutation))
         mutation = start_mutation
         count = 0
 
 
-#Update 'done' variable in request table when done
+#Update 'done' variable in request table when done.
 mutation = f'mutation MyMutation {{update_requests_by_pk(pk_columns: {{request_id: {sys.argv[1]} }}, _set: {{done: true}}) {{done}} }}'
 req_response = client.execute(gql(mutation))
