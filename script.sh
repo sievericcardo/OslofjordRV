1204dec2710e60a587b93ccc181eab3cc9c4415d
nrec <nrec@nrec.com>
2024-04-23 11:00:38 +0000

    monitoring process no longer uses c code

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Running TeSSLa..."
 python3 get_data.py $1 "$2"
-./main
 java -jar tessla.jar interpreter spec.tessla trace.log > output.out
 python3 post_data.py $3
 echo "Done!"
