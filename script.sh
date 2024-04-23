ba3be40d1347c4b64f51fd353007cd196c3c1056
Vebjørn Olsen Leihne <vebjorol@uio.no>
2024-04-23 17:23:20 +0200

    added some echos to help measure progress

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "Running TeSSLa..."
+echo "Running 'get_data.py'..."
 python3 get_data.py $1 "$2"
+echo "Running TeSSLa interpreter..."
 java -jar tessla.jar interpreter spec.tessla trace.log > output.out
+echo "Running 'post_data.py'..."
 python3 post_data.py $3
 echo "Done!"
