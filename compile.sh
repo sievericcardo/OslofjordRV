71253fab40c66bca31c666b627a29ed7e72ff00d
nrec <nrec@nrec.com>
2024-04-23 10:59:38 +0000

    stopped using c code because it made it harder to accomodate missing values in the traces

--- compile.sh
+++
@@ -1,2 +0,0 @@
-java -jar tessla.jar instrumenter spec.tessla main.c /usr/lib/gcc/x86_64-linux-gnu/11/include/
-gcc main.c.instrumented.c -lcjson -llogging -o main
--- main.c.callbacks.h
+++
@@ -1,9 +0,0 @@
-#include "logging.h"
-
-inline __attribute__((always_inline)) void my_func_call(float arg0, int arg1) {
-uint8_t* events = trace_create_events(2);
-trace_push_float(events, "temperature", (double) arg0);
-trace_push_int(events, "id_sim", (int64_t) arg1);
-trace_write(events);
-}
-
