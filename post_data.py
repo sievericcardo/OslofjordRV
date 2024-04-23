e5b7207e989a5df39ccbd58f102f5c53b370de8b
nrec <nrec@nrec.com>
2024-04-23 11:00:16 +0000

    fixed error regarding what values are posted

--- post_data.py
+++ post_data.py
@@ -6,7 +6,7 @@ from gql.transport.aiohttp import AIOHTTPTransport
 
 #Set up GQL with url and headers.
 transport = AIOHTTPTransport(
-        url="http://localhost:8080/v1/graphql",
+    url="http://localhost:8080/v1/graphql",
     headers={"Content-Type":"application/json","x-hasura-admin-secret":"mylongsecretkey"}
 )
 client = Client(transport=transport, fetch_schema_from_transport=True)
@@ -22,15 +22,14 @@ mutation = start_mutation
 
 
 #The length of the monitor output determines how many lines a "row" spans. The output length depends on the info available about the species.
-suitable_temp = 'null'
-suitable_spawn_temp = 'null'
-preferred_spawn_temp = 'null'
+suitable_temp = "false"
+suitable_spawn_temp = "false"
+preferred_spawn_temp = "false"
 id_sim = None
 
 
 #Go through monitor output, build mutation, and post to database.
 count = 0
-i = 1
 for line in lines:
     arr = line.split(" ")
     key = arr[1]
@@ -38,25 +37,23 @@ for line in lines:
 
     if key == "id_sim":
         if count != 0:
-            mutation += f'id_sim: {id_sim}, suitable_temperature: {suitable_temp}, suitable_spawning_temperature: {suitable_spawn_temp}, preferred_spawning_temperature: {preferred_spawn_temp}'
             mutation += '}) {affected_rows}}'
             rv_response = client.execute(gql(mutation))
             mutation = start_mutation
-        id_sim = value
-        count += 1
+        mutation += f'id_sim: {value}, '
 
     elif key == "suitable_temperature":
-        suitable_temp = value
+        mutation += f'suitable_temperature: {value}, '
 
     elif key == "suitable_spawning_temperature":
-        suitabble_spawn_temp = value
+        mutation += f'suitable_spawning_temperature: {value}, '
 
     elif key == "preferred_spawning_temperature":
-        preferred_spawn_temp = value
+        mutation += f'preferred_spawning_temperature: {value}, '
+
+    count += 1
 
 
-#Post the last entry.
-mutation += f'id_sim: {id_sim}, suitable_temperature: {suitable_temp}, suitable_spawning_temperature: {suitable_spawn_temp}, preferred_spawning_temperature: {preferred_spawn_temp}'
 mutation += '}) {affected_rows}}'
 rv_response = client.execute(gql(mutation))
 
