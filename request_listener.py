837bb217880070cf73b542932ac271a1840b1caf
Riccardo Sieve <sievericcardo@gmail.com>
2024-05-30 11:14:03 +0200

    Update request_listener.py
    Added base parameters and port

--- request_listener.py
+++ request_listener.py
@@ -7,6 +7,27 @@ from generator import Generator
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# Base data
+data_parameters = {
+    "name": "fishFields",
+    "field": "FishFields",
+    "base_property": "temperature",
+    "parameters": [
+        ("maxSpawnTemp", "Int"),
+        ("minSpawnTemp", "Int"),
+        ("maxTemp", "Int"),
+        ("minTemp", "Int"),
+        ("prefMaxSpawnTemp", "Int"),
+        ("prefMinSpawnTemp", "Int")
+    ],
+    "species_info": {
+        ("maxTemp", "minTemp"): ["suitable_temperature", "temperature", ">=", "100.0", "<="],
+        ("maxSpawnTemp", "minSpawnTemp"): ["suitable_spawning_temperature", "temperature", ">=", "100.0", "<="],
+        ("prefMaxSpawnTemp", "prefMinSpawnTemp"): ["preferred_spawning_temperature", "temperature", ">=", "100.0", "<="]
+    },
+    "offset": "10.0"
+}
+
 @app.route('/new-request', methods=['POST'])
 def new_request():
     # Confirm the authentication header is correct
@@ -21,19 +42,20 @@ def new_request():
     species_name = data['event']['data']['new']['species_name']
     request_id = data['event']['data']['new']['request_id']
 
-    name = data['event']['data']['new']['name']
-    field = data['event']['data']['new']['field']
-    base_property = data['event']['data']['new']['base_property']
-    parameters = data['event']['data']['new']['parameters']
-    species_info = data['event']['data']['new']['species_info']
-    offset = data['event']['data']['new']['offset']
+    name = data_parameters['name']
+    field = data_parameters['field']
+    base_property = data_parameters['base_property']
+    parameters = data_parameters['parameters']
+    species_info = data_parameters['species_info']
+    offset = data_parameters['offset']
 
     gen = Generator(name, field, parameters, species_info)
     graph_query = gen.generateQuery()
 
     # Save the query into the Oslofjord-DB-API container
     with open(f'/app/{name}.graphql', 'w') as f:
-        f.write(graph_query)
+        # f.write(graph_query)
+        print("Query generated...")
 
     dp = DataProcessor(name, field, base_property, parameters, species_info, offset)
 
@@ -49,5 +71,5 @@ def new_request():
     return jsonify({'message': 'Request received!'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('MONITORING_PORT', 5002))
+    port = int(os.environ.get('MONITORING_PORT', 5001))
     socketio.run(app, host='0.0.0.0', port=port)
